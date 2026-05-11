035e0e34437f5cdec43c06d5928b79c89fb6f9c8
Tanka Prasad Adhikari <afno-github-email@gmail.comaakashpame@gmail.com>
2026-05-11 22:13:52 +0545

    fix: fix infinite redirect loop on homepage

---
+++ .next/static/chunks/app/(public)/about/page-688a72a02ba81ece.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4623,9724,6034,9252,5555,3546,8458,8374],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- .next/static/chunks/app/(public)/election-information/page-c46c9cf9f83a7474.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6004,3386,1398,7428,6156,7874,8296,3681,3367,9802],{9263:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[2972,2971,2117,1744],function(){return n(n.s=9263)}),_N_E=n.O()}]);
